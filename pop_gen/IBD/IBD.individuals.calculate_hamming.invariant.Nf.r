edec3e1ff6db889cae2144fccd58eaea07fbec88
Eric Morrison <emorrison40@gmail.com>
2024-07-30 17:44:43 -0400

    invariant hamming dist

--- pop_gen/IBD/IBD.individuals.calculate_hamming.invariant.Nf.r
+++ pop_gen/IBD/IBD.individuals.calculate_hamming.invariant.Nf.r
@@ -1,7 +1,7 @@
 library(adegenet)
 
 
-dnaBin = adegenet::fasta2DNAbin(file = "~/nf_nd_ham_dist/Nf/FINAL_invariant.snps_only.for_IBD.fasta", chunkSize = 100)
+dnaBin = adegenet::fasta2DNAbin(file = "~/nf_nd_ham_dist/Nf/FINAL_invariant.snps_only.for_IBD.fasta", chunkSize = 25)
 #x nucleotides
 
 dist.raw = ape::dist.dna(x = dnaBin, model = "raw", pairwise.deletion = T)
