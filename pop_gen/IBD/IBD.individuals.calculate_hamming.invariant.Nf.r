a38261582b538328eef94de9b43cd40d6eca1990
Eric Morrison <emorrison40@gmail.com>
2024-08-02 09:50:08 -0400

    speed up invariant dist

--- pop_gen/IBD/IBD.individuals.calculate_hamming.invariant.Nf.r
+++ pop_gen/IBD/IBD.individuals.calculate_hamming.invariant.Nf.r
@@ -1,9 +1,9 @@
 library(adegenet)
 
 
-dnaBin = adegenet::fasta2DNAbin(file = "~/nf_nd_ham_dist/Nf/FINAL_invariant.snps_only.for_IBD.fasta", chunkSize = 25)
+dnaBin = adegenet::fasta2DNAbin(file = "~/nf_nd_ham_dist/Nf/FINAL_invariant.snps_only.for_IBD.fasta", chunkSize = 100) #use high mem node with 100 chunk
 #x nucleotides
 
 dist.raw = ape::dist.dna(x = dnaBin, model = "raw", pairwise.deletion = T)
 
-saveRDS(dist.raw, "~/nf_nd_ham_dist/Nf/hamming_dist.invariant.rds")
+saveRDS(dist.raw, "~/nf_nd_ham_dist/Nf/hamming_dist.invariant.fast_mem.rds")
