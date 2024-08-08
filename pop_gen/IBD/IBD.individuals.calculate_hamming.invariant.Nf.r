d06a3d3382b5a6dc2c0e1533133f5d2ae99bba25
Eric Morrison <emorrison40@gmail.com>
2024-08-08 17:37:41 -0400

    rerun Nf invariant dist

--- pop_gen/IBD/IBD.individuals.calculate_hamming.invariant.Nf.r
+++ pop_gen/IBD/IBD.individuals.calculate_hamming.invariant.Nf.r
@@ -11,10 +11,10 @@ args = commandArgs(trailingOnly=TRUE)
 
 fastaFile = args[1]
 tigName = args[2]
-dnaBin = adegenet::fasta2DNAbin(file = fastaFile, chunkSize = 30) #use high mem node with 100 chunk
+dnaBin = adegenet::fasta2DNAbin(file = fastaFile, chunkSize = 130) #use high mem node with 100 chunk
 
 dist.raw = ape::dist.dna(x = dnaBin, model = "raw", pairwise.deletion = T)
 #dist.raw.len = dist.raw*length(matrix(dnaBin[1,]))
 
-saveRDS(dist.raw, paste(tigName, "raw.rds", sep = ""))
+saveRDS(dist.raw, paste(tigName, "raw.rds", sep = "_"))
 #saveRDS(dist.raw, paste(tigName, "len_corrected.rds", sep = ""))
--- pop_gen/IBD/sum_Nf_invariant_dist.R
+++ pop_gen/IBD/sum_Nf_invariant_dist.R
@@ -4,7 +4,7 @@
 dist_path = "data/Nf/IBD/invariant"
 tig_lens = read.table("data/Nf/IBD/invariant/adegenet.tigs_lens.txt", header = F)
 tig_names = read.table("data/Nf/IBD/invariant/adegenet.tigs_names.txt", header = F)
-dist_files = list.files(path = dist_path, pattern = "len_corrected.rds", full.names = F)
+dist_files = list.files(path = dist_path, pattern = "raw.rds", full.names = F)
 #everytyhing is in the same sort order so we won't sort further
 
 dist_list = list()
@@ -18,10 +18,11 @@ for( i in 2:length(dist_list)){
     sum_dist = sum_dist + dist_list[[i]]
 }
 head(sum_dist)
+sum_dist_kb = sum_dist/1000
 saveRDS(sum_dist, file.path(dist_path, "sum_tigs_len_corrected.rds"))
 range(sum_dist[!is.na(sum_dist)])
 
-range(dist_list[[1]])
+range(dist_list[[2]])
 
 #########################################
 #Nd, len norm distance 
@@ -29,6 +30,17 @@ dist = readRDS("data/Nd/IBD/invariant/hamming_dist.invariant.rds")
 range(dist)
 len = read.table("data/Nd/IBD/invariant/len.txt")
 len
-dist_len = dist*len$V1
+dist_len = dist*(len$V1/1000) #per kilobase
 saveRDS(dist_len, "data/Nd/IBD/invariant/hamming_dist.len_corrected.rds")
+range(dist_len)
+
+
+#########################################
+#Nc, len norm distance 
+dist = readRDS("data/Nc/IBD/invariant/hamming_dist.invariant.rds")
+range(dist)
+len = read.table("data/Nc/IBD/invariant/len.txt")
+len
+dist_len = dist*(len$V1/1000)
+saveRDS(dist_len, "data/Nc/IBD/invariant/hamming_dist.len_corrected.rds")
 range(dist_len)
