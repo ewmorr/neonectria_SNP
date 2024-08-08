ef3cc603b5e382238ea2c41e540cf7a5b0386e2e
Eric Morrison <emorrison40@gmail.com>
2024-08-08 14:58:30 -0400

    rerun Nf invariant dist

--- pop_gen/IBD/IBD.individuals.calculate_hamming.invariant.Nf.r
+++ pop_gen/IBD/IBD.individuals.calculate_hamming.invariant.Nf.r
@@ -14,7 +14,7 @@ tigName = args[2]
 dnaBin = adegenet::fasta2DNAbin(file = fastaFile, chunkSize = 30) #use high mem node with 100 chunk
 
 dist.raw = ape::dist.dna(x = dnaBin, model = "raw", pairwise.deletion = T)
-dist.raw.len = dist.raw*length(matrix(dnaBin[1,]))
+#dist.raw.len = dist.raw*length(matrix(dnaBin[1,]))
 
 saveRDS(dist.raw, paste(tigName, "raw.rds", sep = ""))
-saveRDS(dist.raw, paste(tigName, "len_corrected.rds", sep = ""))
+#saveRDS(dist.raw, paste(tigName, "len_corrected.rds", sep = ""))
---
+++ pop_gen/IBD/sum_Nf_invariant_dist.R
@@ -0,0 +1,34 @@
+
+#########################################
+#Nf, len norm distances and sum over tigs
+dist_path = "data/Nf/IBD/invariant"
+tig_lens = read.table("data/Nf/IBD/invariant/adegenet.tigs_lens.txt", header = F)
+tig_names = read.table("data/Nf/IBD/invariant/adegenet.tigs_names.txt", header = F)
+dist_files = list.files(path = dist_path, pattern = "len_corrected.rds", full.names = F)
+#everytyhing is in the same sort order so we won't sort further
+
+dist_list = list()
+for( i in 1:length(dist_files)){
+    temp.dist = readRDS(file.path(dist_path, dist_files[i]))
+    dist_list[[tig_names[i,1]]] = temp.dist * tig_lens[i,1]
+}
+
+sum_dist = dist_list[[1]]
+for( i in 2:length(dist_list)){
+    sum_dist = sum_dist + dist_list[[i]]
+}
+head(sum_dist)
+saveRDS(sum_dist, file.path(dist_path, "sum_tigs_len_corrected.rds"))
+range(sum_dist[!is.na(sum_dist)])
+
+range(dist_list[[1]])
+
+#########################################
+#Nd, len norm distance 
+dist = readRDS("data/Nd/IBD/invariant/hamming_dist.invariant.rds")
+range(dist)
+len = read.table("data/Nd/IBD/invariant/len.txt")
+len
+dist_len = dist*len$V1
+saveRDS(dist_len, "data/Nd/IBD/invariant/hamming_dist.len_corrected.rds")
+range(dist_len)
