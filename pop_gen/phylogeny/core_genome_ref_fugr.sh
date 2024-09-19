d484f4ef0d8ca46b27b95abd9bdab1d295c297c2
Eric Morrison <emorrison40@gmail.com>
2024-09-19 14:08:55 -0400

    neonectria core genome SPANDx

--- pop_gen/phylogeny/core_genome_ref_fugr.sh
+++ pop_gen/phylogeny/core_genome_ref_fugr.sh
@@ -26,7 +26,20 @@ perl ../../library/collate_nucmer_coords.pl Fusgr1_AssemblyScaffolds_Repeatmaske
 #334314 total positions in core alignment. This ios consistent with the phame paper which found 134062 positions and 40675 SNPs shared in an alignment of E coli Shigella and Salmonella (compared to 2.16M positions ansd 267K SNPs when just comparing E coli and Shigella
 #could consider rerunning this with just the Neonectria
 
+#nf as ref
 cd
 cd repo/neonectria_SNP/data/Nf
 # note that the repeatmasking proposed in phame doesn't actually do anything for Nf
 #need to first run repeatmasker
+
+nucmer --maxmatch --prefix=Nd ref.masked.fasta ../Nd/ref.fasta
+nucmer --maxmatch --prefix=Nc ref.masked.fasta ../Nc/ref.fasta
+
+delta-filter -1 Nd.delta > Nd.one2oneFilter
+delta-filter -1 Nc.delta > Nc.one2oneFilter
+
+show-coords -clTr Nd.one2oneFilter > Nd.coords
+show-coords -clTr Nc.one2oneFilter > Nc.coords
+
+perl ../../library/collate_nucmer_coords.pl ref.masked.fasta Nd.coords Nc.coords
+#neonectria core alignment is 5166727, much more than when fugr included as expected
