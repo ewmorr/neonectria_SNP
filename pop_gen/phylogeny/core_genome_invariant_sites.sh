f88ea277bbeffeea9b2b088a3149af8d0817890f
Eric Morrison <emorrison40@gmail.com>
2024-09-20 10:13:47 -0400

    fugr invariant

--- pop_gen/phylogeny/core_genome_invariant_sites.sh
+++ pop_gen/phylogeny/core_genome_invariant_sites.sh
@@ -2,15 +2,15 @@ cd
 mkdir core_fugr_invariant_sites_GVCF
 mkdir core_fugr_invariant_sites_GVCF/indv_GVCFs
 
-sbatch ~/repo/neonectria_genome_reseq_10072020/premise/indv_GVCFs_array.slurm
+sbatch ~/repo/neonectria_SNP/pop_gen/phylogeny/indv_GVCFs_array.slurm
 
-sbatch ~/repo/neonectria_genome_reseq_10072020/premise/genotype_gvcfs_invariant_sites.slurm
+sbatch ~/repo/neonectria_SNP/pop_gen/phylogeny/genotype_gvcfs_invariant_sites.slurm
 
 cd
 mkdir core_neonectria_invariant_sites_GVCF
 mkdir core_neonectria_invariant_sites_GVCF/indv_GVCFs
 
-sbatch ~/repo/neonectria_genome_reseq_10072020/premise/indv_GVCFs_array.slurm
+sbatch ~/repo/neonectria_SNP/pop_gen/phylogeny/indv_GVCFs_array.slurm
 
-sbatch ~/repo/neonectria_genome_reseq_10072020/premise/genotype_gvcfs_invariant_sites.slurm
+sbatch ~/repo/neonectria_SNP/pop_gen/phylogeny/genotype_gvcfs_invariant_sites.slurm
 
